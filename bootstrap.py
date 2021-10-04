c66583b266c82ea4e840cc8d861abd7d7d44fd48
Roxx <59770086+Gaming-Rowdies@users.noreply.github.com>
2021-10-04 11:13:04 +0530

    Roll back to master branch

--- bootstrap.py
+++ bootstrap.py
@@ -1,102 +1,154 @@
-from os import system, environ, popen
+"""
+Lavalink on Heroku bootstrap script
+Credit to diniboy for sed script
+"""
 
+from os import system, environ, popen
+from version import __build__
 
 class LavalinkBootstrap:
+
+    """
+    Class we're using to get Lavalink working on Heroku
+    """
+    
+    def prepare_version_number(self):
+        
+        self._version_number = popen(
+            
+            """curl --silent "https://api.github.com/repos/Frederikam/Lavalink/releases/latest" | grep -Po '"tag_name": "\K.*?(?=")'"""
+          
+        ).read().strip()
+
     def __init__(self):
-        """Gets latest build number and sets config variables in application.yml"""
 
-        self._version_number = self.prepare_version_number()
-
-        self.download_command = f"curl -L https://ci.fredboat.com/repository/download/Lavalink_Build/{self._version_number}:id/Lavalink.jar?guest=1 -O"
+        """
+        Doing important stuff here
+        """
+        
+        self.prepare_version_number() # Fixes #1
+        
+        self.use_dev_lavalink = True if str(environ.get("USE_DEV_LAVALINK")).lower() not in ("no", "0", "n") else False
+        if self.use_dev_lavalink:
+            
+            print("[INFO] Using developer Lavalink version")
+        
+        self.download_command = f"curl -L https://ci.fredboat.com/repository/download/Lavalink_Build/{__build__}/Lavalink.jar?guest=1 -o Lavalink.jar" if self.use_dev_lavalink else f"curl -L https://github.com/Frederikam/Lavalink/releases/download/{self._version_number}/Lavalink.jar -O"
         print(f"[INFO] Download command: {self.download_command}")
-
+        
         self.replace_port_command = 'sed -i "s|DYNAMICPORT|$PORT|" application.yml'
+
         self.replace_password_command = 'sed -i "s|DYNAMICPASSWORD|$PASSWORD|" application.yml'
         self.replace_password_command_no_password = 'sed -i "s|DYNAMICPASSWORD|youshallnotpass|" application.yml'
-
-        # Heroku provides basic Java configuration based on dyno size, no need in limiting memory
-        self._additional_options = environ.get("ADDITIONAL_JAVA_OPTIONS")
-
-        # User-provided config, will override heroku's
-        self.run_command = f"java -jar Lavalink.jar {self._additional_options}"
-
-
-    def prepare_version_number(self):
-        """Gets latest build number the lavalink ci"""
-
-        builds = (
-            popen(
-                f"""curl --silent https://ci.fredboat.com/repository/download/Lavalink_Build/?guest=1"""
-            )
-            .read()
-            .strip()
-            # removing HTML stuff
-            .replace("https://ci.fredboat.com/repository/download/Lavalink_Build/", "")
-            .replace("</a><br>", "")
-            .split("\n<a href=")
-        )
-
-        build = builds[1][builds[1].find(">") + 1 :] #getting latest build
-        for build in builds:
-            if "tcbuildid" in builds:
-                return build[build.find(">") + 1: build.find(".")]
-
+        
+        self._additional_options = environ.get(
+            "ADDITIONAL_JAVA_OPTIONS"
+        ) # Heroku provides basic Java configuration based on dyno size, no need in limiting memory
+    
+        self.run_command = f"java -jar Lavalink.jar {self._additional_options}" # User-provided config, will override heroku's
 
     def replace_password_and_port(self):
-        """Replacing password and port in application.yml"""
 
-        print("[INFO] Replacing port...")
+        """
+        Replacing password and port in application.yml
+        """
+
+        print(
+            "[INFO] Replacing port..."
+        )
 
         try:
-            system(self.replace_port_command)
+            
+            system(
+                self.replace_port_command
+            )
 
             if not environ.get("PASSWORD"):
+
                 print(
                     """
-                    [WARNING] You have not specified your Lavalink password in config vars. To do this, go to settings and set the PASSWORD environment variable
+                    [WARNING] You have not specified your Lavalink password in config vars. To do this, go to settings 
+                    and set the PASSWORD environment variable
                     """
                 )
-                return system(self.replace_password_command_no_password)
-
-            system(self.replace_password_command)
+    
+                return system(
+                    self.replace_password_command_no_password
+                )
+            
+            system(
+                self.replace_password_command
+            )
 
         except BaseException as exc:
-            print(f"[ERROR] Failed to replace port/password. Info: {exc}")
+
+            print(
+                f"[ERROR] Failed to replace port/password. Info: {exc}"
+            )
 
         else:
-            print("[INFO] Done. Config is ready now")
 
+            print(
+                "[INFO] Done. Config is ready now"
+            )
 
     def download(self):
-        """Downloads latest release of Lavalink"""
 
-        print("[INFO] Downloading latest release of Lavalink...")
+        """
+        Downloads latest release of Lavalink
+        """
 
+        print(
+            "[INFO] Downloading latest release of Lavalink..."
+        )
+        
         try:
-            system(self.download_command)
-
+            
+            system(
+                self.download_command
+            )
+        
         except BaseException as exc:
-            print(f"[ERROR] Lavalink download failed. Info: {exc}")
+
+            print(
+                f"[ERROR] Lavalink download failed. Info: {exc}"
+            )
 
         else:
-            print("[INFO] Lavalink download OK")
-
-
+        
+            print(
+                "[INFO] Lavalink download OK"
+            )
+    
     def run(self):
-        """Runs Lavalink instance"""
+
+        """
+        Runs Lavalink instance
+        """
 
         self.download()
         self.replace_password_and_port()
 
-        print("[INFO] Starting Lavalink...")
+        print(
+            "[INFO] Starting Lavalink..."
+        )
 
         try:
-            system(self.run_command)
-        except BaseException as exc:
-            print(f"[ERROR] Failed to start Lavalink. Info: {exc}")
 
+            system(
+                self.run_command
+            )
+        
+        except BaseException as exc:
+
+            print(
+                f"[ERROR] Failed to start Lavalink. Info: {exc}"
+            )
 
 if __name__ == "__main__":
-    """Starts our instance"""
+
+    """
+    Starts our instance
+    """
 
     LavalinkBootstrap().run()
